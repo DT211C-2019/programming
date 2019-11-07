1a06a87e793994c3aefdef5914ed120ba4cf5690
Solonerus <Solonerus@gmail.com>
2019-11-07 12:14:22 +0000

    Additional framework for V2

--- Saul Burgess/Assignments/AS1_MathQuestion/AS1_MathQuestion.c
+++ Saul Burgess/Assignments/AS1_MathQuestion/AS1_MathQuestion.c
@@ -5,18 +5,31 @@ Compiler: GCC (mingw)
 OS: Windows 10
 */
 
+//Libraries.
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+
+//Definitions.
+#define maxquestions 5
 
 
 int main(){
   int program_main = 1;
+  int q3lock = 1;
+  int i;
+  int j;
   int function_select;
+
+  char operators[4] = {'+','-','*','/'};
+  int former[maxquestions];
+  int latter[maxquestions];
+  int answer[maxquestions];
 
 
   while (program_main == 1){
-    printf("Welcome to the MathQuiz program!\nPlease select one of the options:\n1. TEMP\n2. TEMP\n3. TEMP\n4. Terminate the program.");
+    printf("Welcome to the MathQuiz program!\nPlease select one of the options:\n\n1. Edit Quiz Length\n2. Begin Quiz\n3. Display Results\n4. Terminate the program.\n");
     scanf("%1s", &function_select);
 
 
@@ -34,17 +47,22 @@ int main(){
 
       case '3' : {
         printf("You entered 3.");
+        
         break;
       }//End Case 3.
 
       case '4' : {
-        printf("You have now terminated the program. Goodbye.");
-        sleep(1);
+        printf("\n\n=============================================\nYou have now terminated the program. Goodbye\n=============================================");
+        sleep(2.5);
         program_main = 0;
         break;
       }//End Case 4.
 
 
+      default :{
+        printf("\n\n=============================================================================================\nInvalid Operator Entered. Please select a valid operator from the screen above when prompted.\n=============================================================================================");
+        sleep(3.5);
+      }//End Default.
 
 
     }//End Switch.
