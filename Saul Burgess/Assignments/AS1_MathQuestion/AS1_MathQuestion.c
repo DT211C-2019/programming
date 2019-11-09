7f1411b0af9fd34a12d1551494bcdb00dfe62d97
Saul Burgess <Solonerus@gmail.com>
2019-11-09 18:47:17 +0000

    Updates assignment, will do some more later.

--- Saul Burgess/Assignments/AS1_MathQuestion/AS1_MathQuestion.c
+++ Saul Burgess/Assignments/AS1_MathQuestion/AS1_MathQuestion.c
@@ -17,46 +17,79 @@ OS: Windows 10
 
 int main(){
   int program_main = 1;
-  int q3lock = 1;
-  int q2lock = 1;
+  int q3lock, q2lock = 0;
   int i;
   int j;
   int function_select;
+  int questionsnormal = maxquestions;
 
   char operators[4] = {'+','-','*','/'};
   int former[maxquestions];
   int latter[maxquestions];
   int answer[maxquestions];
+  int Uanswer[maxquestions];
 
 
   while (program_main == 1){
     printf("Welcome to the MathQuiz program!\nPlease select one of the options:\n\n1. Edit Quiz Length\n2. Begin Quiz\n3. Display Results\n4. Terminate the program.\n");
-    scanf("%1s", &function_select);
+    scanf("%d", &function_select);
+    getchar();
 
 
     switch (function_select){
 
-      case '1' : {
-        printf("You entered 1.");
+      case 1 : {
+        printf("\nPlease enter the number of questions you wish to be asked. The maximum is %d\n", maxquestions);
+        scanf(" %d", &questionsnormal);
+        getchar();
+
+        q2lock = 1;
+
+        for(i=0;i>questionsnormal;i++){
+          former[i] = rand() % 10 + 1;
+          latter[i] = rand() % 10 + 1;
+          answer[i] = latter[i] + former[i];
+        }//End for.
+
         break;
       }//End Case 1.
 
-      case '2' : {
-        printf("You entered 2.");
-        break;
-      }//End Case 2.
+      case 2 : {
+        if(q2lock == 1){
 
-      case '3' : {
-        if(q3lock == 0){
+          if(questionsnormal>maxquestions){
+            questionsnormal = maxquestions;
+            printf("You have entered either: A number greater then %d or a Character in option 1. Your selection has been defaulted to %d", maxquestions, maxquestions);
+            sleep(4.5);
+          }//End if.
+
+          for(i=0;i>questionsnormal;i++){
+            printf("\nThe Quiz will now start. You have three seconds to prepare");
+            sleep(4.5);
+            printf("\n%d + %d\n", former[i], latter[i]);
+            scanf("%d", Uanswer[i]);
+          }//End for.
 
         }//End If.
         else{
-          printf("Invalid Input. Quiz must be played to display results.\n");
+          printf("Invalid Input. Quiz cannot be started without entering the number of questions.");
+          sleep(2.5);
+        }//End Else.
+        break;
+      }//End Case 2.
+
+      case 3 : {
+        if(q3lock == 1){
+
+        }//End If.
+        else{
+          printf("Invalid Input. Quiz must be played to display results.");
+          sleep(2.5);
         }//End Else.
         break;
       }//End Case 3.
 
-      case '4' : {
+      case 4 : {
         printf("You have now terminated the program. Goodbye\n");
         sleep(2.5);
         program_main = 0;
