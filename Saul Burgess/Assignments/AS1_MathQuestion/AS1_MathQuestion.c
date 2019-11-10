a86441a93eb15bb78604ad3c994d6d625222dac8
Saul Burgess <Solonerus@gmail.com>
2019-11-10 21:10:28 +0000

    Completes V5 of my program for upload.

--- Saul Burgess/Assignments/AS1_MathQuestion/AS1_MathQuestion.c
+++ Saul Burgess/Assignments/AS1_MathQuestion/AS1_MathQuestion.c
@@ -1,6 +1,6 @@
 /*
 Summary: To create a program that simulates a maths quiz.
-Date of Last Edit: 23.10.19
+Date of Last Edit: 10/11/19
 Compiler: GCC (mingw)
 OS: Windows 10
 */
@@ -16,95 +16,175 @@ OS: Windows 10
 
 
 int main(){
-  int program_main = 1;
-  int q3lock, q2lock = 0;
-  int i;
-  int j;
-  int function_select;
+  //Sets the seed of the random generator to the output given by time.h
+  srand(time(NULL));
+
+  //Locks the program down, preventing segments from being triggered until the approriate time.
+  char program_main = 'Y';
+  int q2lock = 0;
+  int q3lock = 0;
+
+  //Limits for random numbers.
+  int max_q=10;
+  int max_char=4;
+
+  int i = 0;
+  int function_select, char_select, operator, qcounter, correct, incorrect;
   int questionsnormal = maxquestions;
-
-  char operators[4] = {'+','-','*','/'};
-  int former[maxquestions];
-  int latter[maxquestions];
-  int answer[maxquestions];
-  int Uanswer[maxquestions];
+  float latter, former, ans, Usrans;
 
 
-  while (program_main == 1){
+  //Global while loop, will continue to loop the program until it is terminated by function 4.
+  while (program_main == 'Y'){
     printf("Welcome to the MathQuiz program!\nPlease select one of the options:\n\n1. Edit Quiz Length\n2. Begin Quiz\n3. Display Results\n4. Terminate the program.\n");
     scanf("%d", &function_select);
     getchar();
 
+    //Defaults rand() limits.
+    former = latter = 0;
+    ans = Usrans = 0;
+
 
     switch (function_select){
 
+      //This allows the user to set the total number of questions, the program stop any numbers greater then maxquestions, defaulting them to maxquestions if that occurs.
+      //NOTE: As the int is already defined, any chars entered will be caught by the getchar(). Meaning questionsnormal will be defaulted to maxquestions
       case 1 : {
-        printf("\nPlease enter the number of questions you wish to be asked. The maximum is %d\n", maxquestions);
+        printf("\n\n===============================================================================================\n");
+        printf("Please enter the number of questions you wish to be asked. The current maximum is %d\n", maxquestions);
+        printf("Warning: If you select a character or a number greater then %d, the program will default to %d.\n", maxquestions, maxquestions);
+        printf("===============================================================================================\n");
         scanf(" %d", &questionsnormal);
         getchar();
 
+        if(questionsnormal>maxquestions){
+          questionsnormal = maxquestions;
+        }//End if.
         q2lock = 1;
-
-        for(i=0;i>questionsnormal;i++){
-          former[i] = rand() % 10 + 1;
-          latter[i] = rand() % 10 + 1;
-          answer[i] = latter[i] + former[i];
-        }//End for.
-
         break;
       }//End Case 1.
 
+
+      //This case governs the quiz section, it will randomly generate the former and latter, then randomly generate the operator.
+      //It will then detect if it is correct or incorrect, and add them to the tally.
       case 2 : {
-        if(q2lock == 1){
 
-          if(questionsnormal>maxquestions){
-            questionsnormal = maxquestions;
-            printf("You have entered either: A number greater then %d or a Character in option 1. Your selection has been defaulted to %d", maxquestions, maxquestions);
-            sleep(4.5);
-          }//End if.
+        //Defaults counters required for for loop.
+        qcounter = 1;
+        correct = incorrect = 0;
 
-          for(i=0;i>questionsnormal;i++){
-            printf("\nThe Quiz will now start. You have three seconds to prepare");
-            sleep(4.5);
-            printf("\n%d + %d\n", former[i], latter[i]);
-            scanf("%d", Uanswer[i]);
+        //Prevents activation if case 1 is not triggered.
+        if (q2lock == 1){
+
+          //Allows case 3 to be triggered.
+          q3lock = 1;
+
+          for (i=1; i<=questionsnormal; i++){
+
+            //Generates former and latter.
+            former = rand()%max_q;
+            latter = rand()%max_q;
+
+            //Generates random operator.
+            char_select = rand()%max_char;
+            switch (char_select){
+
+              case 1 : {
+                operator = '*';
+                ans = (former*latter);
+                break;
+              }//End *
+
+              case 2 : {
+                operator = '+';
+                ans = (former+latter);
+                break;
+              }//End +
+
+              case 3 : {
+                operator = '-';
+                ans = (former-latter);
+                break;
+              }//End -
+
+              case 4 : {
+                operator = '/';
+                ans = (former/latter);
+              }//End /
+            }//End Switch.
+
+            //prints the question, then requests an answer from user.
+            printf("\n%d: %.2f %c %.2f\n", i, former, operator, latter);
+            scanf(" %f", &Usrans);
+            getchar();
+
+            if (ans == Usrans){
+              correct++;
+              printf("You are correct!");
+              sleep(1);
+            }//End CORRECT if.
+
+            if (ans != Usrans){
+              incorrect++;
+              printf("You are incorrect");
+              sleep(1);
+            }//End INCORRECT if.
+            printf("\n\n");
+
           }//End for.
-
         }//End If.
-        else{
-          printf("Invalid Input. Quiz cannot be started without entering the number of questions.");
+        else {
+          printf("=========================================================\n");
+          printf("You must input a question limit before starting the quiz.\n");
+          printf("=========================================================\n");
           sleep(2.5);
         }//End Else.
-        break;
       }//End Case 2.
 
+
+      //Case 3 displays the answer when requested by the user.
       case 3 : {
+        //This statement prevents the case from being triggered if the quiz is not completed.
         if(q3lock == 1){
+          printf("=============\n");
+          printf("Correct: %d\n", correct);
+          printf("Incorrect: %d\n", incorrect);
+          printf("=============\n");
+          sleep(2.5);
 
         }//End If.
         else{
-          printf("Invalid Input. Quiz must be played to display results.");
+          printf("======================================================\n");
+          printf("Invalid Input. Quiz must be played to display results.\n");
+          printf("======================================================\n");
           sleep(2.5);
         }//End Else.
+
         break;
       }//End Case 3.
 
+
+      //Terminates the program when this option is selected by making the while loop false.
       case 4 : {
+        printf("============================================\n");
         printf("You have now terminated the program. Goodbye\n");
+        printf("============================================\n");
         sleep(2.5);
-        program_main = 0;
+        program_main = 'N';
         break;
       }//End Case 4.
 
-
+      //Catches any illegal intigers.
       default :{
+        printf("=============================================================================================\n");
         printf("Invalid Operator Entered. Please select a valid operator from the screen above when prompted.\n");
-        sleep(3.5);
+        printf("=============================================================================================\n");
+        sleep(2.5);
       }//End Default.
-
 
     }//End Switch.
 
+    //Primairly for neateness, clears the screen on each loop.
     system("cls");
   }//End While.
 
